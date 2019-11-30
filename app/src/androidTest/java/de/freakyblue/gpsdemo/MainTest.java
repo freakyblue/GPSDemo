182499051f04497f9e53e6b85df04c4c903d964c
jonny <github@jonny-seitz.de>
2019-11-30 04:48:34 +0100

    Add demo gps tracker

---
+++ app/src/androidTest/java/de/freakyblue/gpsdemo/MainTest.java
@@ -0,0 +1,84 @@
+package de.freakyblue.gpsdemo;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import androidx.test.rule.ActivityTestRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class MainTest {
+
+    private Context context;
+    private LocationManager locationManager;
+
+    @Rule
+    public ActivityTestRule<MainActivity> testRule =
+        new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void before() {
+        context = InstrumentationRegistry
+            .getInstrumentation()
+            .getTargetContext();
+
+        locationManager = (LocationManager) context
+            .getSystemService(Context.LOCATION_SERVICE);
+        locationManager.addTestProvider(
+            LocationManager.GPS_PROVIDER,
+            false,
+            false,
+            false,
+            false,
+            true,
+            true,
+            true,
+            Criteria.NO_REQUIREMENT,
+            0
+        );
+        locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+
+        updateLocation(0, 0, 100);
+    }
+
+    private void updateLocation(double lat, double lng, double alt) {
+        locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+        Location location = new Location(LocationManager.GPS_PROVIDER);
+        location.setLatitude(lat);
+        location.setLongitude(lng);
+        location.setAltitude(alt);
+        location.setAccuracy(0);
+        location.setVerticalAccuracyMeters(1);
+        location.setSpeedAccuracyMetersPerSecond(1);
+        location.setBearingAccuracyDegrees(2);
+        location.setTime(System.currentTimeMillis());
+        location.setElapsedRealtimeNanos(System.nanoTime() + 200);
+        locationManager.setTestProviderLocation(
+            LocationManager.GPS_PROVIDER,
+            location
+        );
+        locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+    }
+
+    @Test
+    public void testTracking() throws InterruptedException {
+        Thread.sleep(4000);
+        updateLocation(0,50,200);
+        Thread.sleep(2000);
+    }
+
+}
--- app/src/main/java/de/freakyblue/gpsdemo/MainActivity.java
+++ app/src/main/java/de/freakyblue/gpsdemo/MainActivity.java
@@ -1,13 +1,126 @@
 package de.freakyblue.gpsdemo;
 
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
+
+import android.annotation.SuppressLint;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements OnMapReadyCallback, LocationListener {
+
+    private MapView map;
+    private TextView text;
+    private GoogleMap gmap;
+    private LocationManager locationManager;
+    private Location prevLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map = findViewById(R.id.map);
+        text = findViewById(R.id.text);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Bundle bundle = null;
+        if (savedInstanceState != null) {
+            bundle = savedInstanceState.getBundle("MAP_BUNDLE_KEY");
+        }
+        map.onCreate(bundle);
+        map.getMapAsync(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        map.onStart();
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+        if (gmap != null)
+            gmap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(0,0)));
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        locationManager.requestLocationUpdates(
+            locationManager.getBestProvider(criteria, true),
+            1000,
+            10,
+            this
+        );
+    }
+
+    @Override
+    public void onPause() {
+        map.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        map.onStop();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        text.setText("test");
+        LatLng currPos = new LatLng(location.getLatitude(), location.getLongitude());
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(currPos));
+        gmap.addMarker(new MarkerOptions().position(currPos).title("marker"));
+        if (prevLocation != null) {
+            text.setText(String.valueOf(location.distanceTo(prevLocation)));
+        }
+        prevLocation = location;
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+    @Override
+    public void onProviderEnabled(String s) {}
+
+    @Override
+    public void onProviderDisabled(String s) {}
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMyLocationEnabled(true);
+        gmap.setMapType(MAP_TYPE_SATELLITE);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(0,0)));
     }
 }
